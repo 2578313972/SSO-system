da21711a782300f4903dcac03f30b749424b82bc
2578313972 <2578313972@qq.com>
2021-08-26 01:20:12 +0800

    nest initialize

--- server/src/routers/index.ts
+++
@@ -1,7 +0,0 @@
-import Router from 'koa-router'
-import register from "./register";
-const router:Router = new Router()
-
-router.use(register.routes())
-
-export default router
--- server/src/types/register.ts
+++
@@ -1,8 +0,0 @@
-/* export function IgenerateJWT(
-  payload: string | Buffer | object,
-  time?: string | number
-): string */
-
-export interface IgenerateJWT {
-  (payload: string | Buffer | object, time?: string | number): string
-}
